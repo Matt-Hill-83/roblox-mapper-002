3883063e5d1edd3a9dbca6701001c87d1ae6469c
BadBeatBuck <matt.hill.email@gmail.com>
2025-07-02 00:40:20 -0700

    cu

--- src/shared/modules/barMaker.ts
+++ src/shared/modules/barMaker.ts
@@ -1,3 +1,5 @@
+import { createTextBox } from "./TextBoxMaker";
+
 interface BarProps {
   Size: [number, number, number];
   Anchored: boolean;
@@ -33,26 +35,6 @@ const defaultProps: BarProps = {
 
 const pointSize = 0.1;
 
-function makeSurfaceGui(face: string, label: string, blockColor: [number, number, number]) {
-  const surfaceGui = new Instance("SurfaceGui");
-  surfaceGui.Face = face as unknown as Enum.NormalId;
-  surfaceGui.SizingMode = Enum.SurfaceGuiSizingMode.PixelsPerStud;
-  surfaceGui.PixelsPerStud = 50;
-
-  const textBox = new Instance("TextBox");
-  textBox.Text = label;
-  textBox.TextSize = 24;
-  textBox.Font = Enum.Font.SourceSans;
-  textBox.Size = new UDim2(1, 0, 1, 0);
-  textBox.BackgroundColor3 = Color3.fromRGB(blockColor[0] * 255, blockColor[1] * 255, blockColor[2] * 255);
-  textBox.TextColor3 = Color3.fromRGB(0, 0, 0);
-  textBox.BorderSizePixel = 10;
-  textBox.TextWrapped = true;
-  textBox.Parent = surfaceGui;
-
-  return surfaceGui;
-}
-
 function makeAttachment(name: string, offset: number, id: string) {
   const attachment = new Instance("Attachment");
   attachment.Name = id;
@@ -60,7 +42,13 @@ function makeAttachment(name: string, offset: number, id: string) {
   return attachment;
 }
 
-function makeCircle(name: string, x: number, y: number, z: number, color: [number, number, number]) {
+function makeCircle(
+  name: string,
+  x: number,
+  y: number,
+  z: number,
+  color: [number, number, number]
+) {
   const circle = new Instance("Part");
   circle.Size = new Vector3(pointSize, pointSize, pointSize);
   circle.Position = new Vector3(x, y, z);
@@ -80,22 +68,26 @@ function padNumber(num: number, length: number): string {
   return result;
 }
 
-function generateBarName(stackIndex: number, hexIndex: number, barIndex: number): string {
+function generateBarName(
+  stackIndex: number,
+  hexIndex: number,
+  barIndex: number
+): string {
   const stackStr = padNumber(stackIndex, 3);
   const hexStr = padNumber(hexIndex, 3);
   const barStr = padNumber(barIndex, 3);
   return `bar${barStr}-h${hexStr}-st${stackStr}`;
 }
 
-export function makeBar({ 
-  id, 
-  position = { x: 0, y: 0, z: 0 }, 
-  rotation = { x: 0, y: -30, z: 0 }, 
-  props = {}, 
+export function makeBar({
+  id,
+  position = { x: 0, y: 0, z: 0 },
+  rotation = { x: 0, y: -30, z: 0 },
+  props = {},
   label = "Bar",
   stackIndex = 1,
   hexIndex = 1,
-  barIndex = 1
+  barIndex = 1,
 }: BarConfig): Part {
   const finalProps = {
     ...defaultProps,
@@ -120,33 +112,73 @@ export function makeBar({
   const barName = generateBarName(stackIndex, hexIndex, barIndex);
   const bar = new Instance("Part");
   bar.Name = barName;
-  bar.Size = new Vector3(finalProps.Size[0], finalProps.Size[1], finalProps.Size[2]);
+  bar.Size = new Vector3(
+    finalProps.Size[0],
+    finalProps.Size[1],
+    finalProps.Size[2]
+  );
   bar.Position = new Vector3(position.x, position.y, position.z);
   bar.Orientation = new Vector3(rotation.x, rotation.y, rotation.z);
   bar.Anchored = finalProps.Anchored;
-  bar.Color = Color3.fromRGB(blockColor[0] * 255, blockColor[1] * 255, blockColor[2] * 255);
+  bar.Color = Color3.fromRGB(
+    blockColor[0] * 255,
+    blockColor[1] * 255,
+    blockColor[2] * 255
+  );
   bar.Material = finalProps.Material as unknown as Enum.Material;
   bar.TopSurface = finalProps.TopSurface as unknown as Enum.SurfaceType;
   bar.BottomSurface = finalProps.BottomSurface as unknown as Enum.SurfaceType;
   bar.Transparency = finalProps.Transparency;
 
-  const frontAttachment = makeAttachment("FrontAttachment", frontFaceOffset, `att${padNumber((barIndex - 1) * 2 + 1, 3)}-h${padNumber(hexIndex, 3)}-st${padNumber(stackIndex, 3)}`);
-  const backAttachment = makeAttachment("BackAttachment", backFaceOffset, `att${padNumber((barIndex - 1) * 2 + 2, 3)}-h${padNumber(hexIndex, 3)}-st${padNumber(stackIndex, 3)}`);
-  
+  const frontAttachment = makeAttachment(
+    "FrontAttachment",
+    frontFaceOffset,
+    `att${padNumber((barIndex - 1) * 2 + 1, 3)}-h${padNumber(
+      hexIndex,
+      3
+    )}-st${padNumber(stackIndex, 3)}`
+  );
+  const backAttachment = makeAttachment(
+    "BackAttachment",
+    backFaceOffset,
+    `att${padNumber((barIndex - 1) * 2 + 2, 3)}-h${padNumber(
+      hexIndex,
+      3
+    )}-st${padNumber(stackIndex, 3)}`
+  );
+
   frontAttachment.Parent = bar;
   backAttachment.Parent = bar;
 
-  const frontSurfaceGui = makeSurfaceGui("Front", label, blockColor);
-  const backSurfaceGui = makeSurfaceGui("Back", label, blockColor);
-  
-  frontSurfaceGui.Parent = bar;
-  backSurfaceGui.Parent = bar;
+  createTextBox({
+    part: bar,
+    face: Enum.NormalId.Front,
+    text: label,
+  });
 
-  const frontCircle = makeCircle("FrontCircle", frontX, position.y, frontZ, [0, 1, 0]);
-  const backCircle = makeCircle("BackCircle", backX, position.y, backZ, [1, 0, 0]);
-  
+  createTextBox({
+    part: bar,
+    face: Enum.NormalId.Back,
+    text: label,
+  });
+
+  const frontCircle = makeCircle(
+    "FrontCircle",
+    frontX,
+    position.y,
+    frontZ,
+    [0, 1, 0]
+  );
+  const backCircle = makeCircle(
+    "BackCircle",
+    backX,
+    position.y,
+    backZ,
+    [1, 0, 0]
+  );
+
   frontCircle.Parent = bar;
   backCircle.Parent = bar;
 
   return bar;
-} 
+}
